9871a39b348a8885d05dd9ade5c355cbd42db4af
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2020-08-25 22:16:26 -0700

    Exhaustive search code MATLAB

---
+++ ExhaustiveSearchMat/generate_video.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+search_dir="./"
+for entry in "$search_dir"/*
+do
+  #echo "$entry" | cut -d'/' -f 3
+  SUBSTR=$(echo $entry | cut -d'/' -f 3)
+  
+  command=$(echo ffmpeg -framerate 1 -pattern_type glob -i \'${SUBSTR}/*.png\' ${SUBSTR}_SignalPos_SignalLen.mp4)
+  echo $command >> ffpmeg
+done
+
