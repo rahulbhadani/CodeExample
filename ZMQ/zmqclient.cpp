68a0b9b738111a0b05d2bfdd7d52ec1cde910ba0
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2022-12-03 16:25:43 -0600

    Added ZEROMQ code

---
+++ ZMQ/zmqclient.cpp
@@ -0,0 +1,39 @@
+//
+//  Hello World client
+//  Connects REQ socket to tcp://localhost:5555
+//  Sends "Hello" to server, expects "World" back
+//
+#include <zmq.hpp>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main (int argc, char **argv)
+{
+    void *context = zmq_ctx_new ();
+
+    //  Socket to talk to server
+    printf ("Connecting to hello world server...\n");
+    void *requester = zmq_socket (context, ZMQ_REQ);
+    zmq_connect (requester, "tcp://localhost:5555");
+
+    int request_nbr;
+    for (request_nbr = 0; request_nbr != 10; request_nbr++) {
+        zmq_msg_t request;
+        zmq_msg_init_size (&request, 15);
+        memcpy (zmq_msg_data (&request), argv[1], 15);
+        printf ("Sending Hello %d...\n", request_nbr);
+        zmq_msg_send (&request, requester, 0);
+        zmq_msg_close (&request);
+
+        zmq_msg_t reply;
+        zmq_msg_init (&reply);
+        zmq_msg_recv (&reply, requester, 0);
+        printf ("Received World %d\n", request_nbr);
+        zmq_msg_close (&reply);
+    }
+    sleep (2);
+    zmq_close (requester);
+    zmq_ctx_destroy (context);
+    return 0;
+}
---
+++ ZMQ/zmqserver.cpp
@@ -0,0 +1,54 @@
+//
+//  Hello World server in C++
+//  Binds REP socket to tcp://*:5555
+//  Expects "Hello" from client, replies with "World"
+//
+#include <zmq.hpp>
+#include <string>
+#include <iostream>
+#include <unistd.h>
+
+int main () 
+{
+    //  Prepare our context and socket
+    zmq::context_t context (1);
+    zmq::socket_t socket (context, ZMQ_REP);
+    socket.bind ("tcp://*:5555");
+
+    while (true) 
+    {
+        zmq::message_t request;
+
+        //  Wait for next request from client
+        //socket.recv (&request);
+
+        zmq_msg_t message;
+        zmq_msg_init (&message);
+
+        int size = zmq_msg_recv (&message, socket, 0);
+        if (size == -1)
+        {
+            std::cout <<"size -1";
+        }
+        else
+        {
+
+            char *strr = (char*)malloc (size + 1);
+            memcpy (strr, zmq_msg_data (&message), size);
+            zmq_msg_close (&message);
+            strr [size] = 0;
+
+            std::cout << strr<<" --- "<<std::endl;
+        }
+        std::cout << "Received Hello" << std::endl;
+
+        //  Do some 'work'
+        sleep (1);
+
+        //  Send reply back to client
+        zmq::message_t reply (5);
+        memcpy ((void *) reply.data (), "World", 5);
+        socket.send (reply);
+    }
+    return 0;
+}
